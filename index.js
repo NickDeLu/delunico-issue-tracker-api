377afd7bd11adc0a270cd995e89717ffee2b0d02
Nick <delunico@sheridancollege.ca>
2022-03-03 17:12:00 -0500

    removed path, added base url messahe

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const app = express()
-const path = require('path')
 var bodyParser = require('body-parser')
 require('dotenv').config();
 const taskRoutes = require('./routes/taskRoutes');
@@ -13,6 +12,8 @@ app.use(bodyParser.json())
 app.use('/tasks', taskRoutes);
 app.use('/projects', projectRoutes);
 app.use('/status', statusRoutes);
+
+app.get("/", (req, res) => res.send("hello, welcome to issue tracker api"))
 
 app.listen(process.env.PORT || 5000, () => {
     console.log(`issue-tracker-api listening at http://localhost:${process.env.PORT || 5000}`)
